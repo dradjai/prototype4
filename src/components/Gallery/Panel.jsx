9ef74272e00a1d50a58dc345ad195ca246b49e21
David Radjai <dradjai@yahoo.com>
2023-06-03 18:33:52 -0400

    added modal to home page for each item

--- src/components/Gallery/Panel.jsx
+++ src/components/Gallery/Panel.jsx
@@ -1,14 +1,37 @@
-import { Col, Image } from "react-bootstrap";
+import { useState } from "react";
+import { Col, Container, Row, Image, Modal } from "react-bootstrap";
 
 export default function Panel({element: {title, content, image}}) {
+
+  const [show, setShow] = useState(false);
+  const toggleShow = () => setShow(!show);
+
 
   return(
 
     <Col xs={11} md={6} lg={3}>
-      <div>
+      <div onClick={toggleShow}>
         <Image src={image} fluid/>
         <h3>{title}</h3>
       </div>
+
+      <Modal show={show} onHide={toggleShow} size="lg" centered>
+      
+        <Modal.Body>
+          <Container>
+            <Row>
+              <Col>
+                <Image src={image} fluid/>
+              </Col>
+              
+              <Col xs={12} md={6}>
+                <h3>{title}</h3>
+                <p>{content}</p>
+              </Col>
+            </Row>
+          </Container>
+        </Modal.Body>
+      </Modal>
 
     </Col>
     
